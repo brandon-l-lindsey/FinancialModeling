b37c1b50922476915985e1ee33664bbd4db79248
brandon-l-lindsey <branllindsey@gmail.com>
2020-05-31 19:18:37 -0600

    i got it! able to search by ticker for a specific company's historical stock data

--- app.js
+++ app.js
@@ -51,93 +51,88 @@ myApp.controller('TickerController', ['$scope', '$http', function($scope, $http)
 myApp.controller('GraphController', ['$scope', '$http', function($scope, $http){
     
     $scope.APIKey = 'H5XW676HRUVKHEFX';    
-    
-//    $http({
-//      method: 'GET',
-//      url: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=IBM&apikey='+$scope.APIKey
-//   }).then(function (result){
-//        $scope.tickers = result.data;
-//   });
+ 
+    // $scope.getData = function() {
+    //     $http({
+    //         method: 'GET',
+    //         url: 'json/testPrices.json'
+    //         // url: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol='+$scope.company+'&apikey='+$scope.APIKey
+    //      }).then(function (result){
+    //           $scope.messyData = result.data["Monthly Adjusted Time Series"];
+    //           var xValues = [];
+    //           var yValues = [];
+    //           angular.forEach($scope.messyData, function(value, key) {
+    //               var adjustedDate = $scope.monthMap(key.slice(5,7)) + '-' + key.slice(0,4);
+    //               xValues.unshift(adjustedDate);
+    //               yValues.unshift(value["5. adjusted close"]);
+    //           });
+    //           $scope.xVals = xValues;
+    //           $scope.yVals = yValues;
+    //           console.log("got Data");
+    //           console.log(xValues);
 
-    
+    //      });
+    // }
 
-   
+         
 
-    
+
+    $scope.newSearch = function(company){
+        $scope.company = company;
         $http({
             method: 'GET',
-            // url: 'json/testPrices.json'
-            url: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol=IBM&apikey='+$scope.APIKey
+            //url: 'json/testPrices.json'
+            url: 'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol='+$scope.company+'&apikey='+$scope.APIKey
          }).then(function (result){
               $scope.messyData = result.data["Monthly Adjusted Time Series"];
               var xValues = [];
               var yValues = [];
               angular.forEach($scope.messyData, function(value, key) {
-                  var adjustedDate = key.slice(5,8) + key.slice(0,4);
+                  var adjustedDate = $scope.monthMap(key.slice(5,7)) + '-' + key.slice(0,4);
                   xValues.unshift(adjustedDate);
                   yValues.unshift(value["5. adjusted close"]);
               });
-              $scope.labels2 = xValues;
-              $scope.data2 = yValues;
-              console.log($scope.xVals);
-              console.log($scope.yVals);
+              $scope.xVals = xValues;
+              $scope.yVals = yValues;
+              console.log("got Data");
+              console.log(xValues);
 
-
+         }).then(function(){
+            new Chart(document.getElementById("line-chart"), {
+                type: 'line',
+                data: {
+                    labels: $scope.xVals,
+                    datasets: [{
+                        data: $scope.yVals,
+                        label: 'Historical Monthly Adjusted Closing Prices for ' + $scope.company,
+                        borderColor: 'blue',
+                        fill: true,
+                        cubicInterpolationMode: 'default',
+                        pointRadius: 0,
+                        pointHoverRadius: 5
+                    }]
+                },
+                options: {
+                    legend: {
+                        display: true,
+                        position: 'bottom'
+                    },
+                    title: {
+                        display: true,
+                        text: 'Historical Adjusted Monthly Close Prices for ' + $scope.company
+                    }
+                }
+            })
          });
-         
-    
+        console.log('success');
         
+    }
 
+    $scope.monthMap = function(month) {
+        return $scope.months[parseInt(month) - 1];
+    }
 
-
-    //  $scope.data = {
-    //      labels: $scope.xValues,
-    //      datasets: [{
-    //          label: "Historical Monthly Adjusted Stock Prices",
-    //          data: $scope.yValues,
-    //          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    //          borderColor: 'rgba(255, 99, 132, 1)',
-    //          borderWidth: 1,
-    //          fill: false
-    //      }]
-    //  };
-
-
-    // $scope.labels2 = ["January", "February", "March", "April", "May", "June", "July"];
-    // $scope.data2 = [
-    //     [$scope.yVals],
-    //     [28, 48, 40, 19, 86, 27, 90]
-    // ];
-    $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-    };
-    $scope.options2 = {
-        scales: {
-        yAxes: [
-            {
-            id: 'y-axis-1',
-            type: 'linear',
-            display: true,
-            position: 'left',
-            }
-        ]
-        }
-    };
-
-    $scope.colors = [
-        {
-          backgroundColor: "rgba(159,204,0, 0.2)",
-          pointBackgroundColor: "rgba(159,204,0, 1)",
-          pointHoverBackgroundColor: "rgba(159,204,0, 0.8)",
-          borderColor: "rgba(159,204,0, 1)",
-          pointBorderColor: '#fff',
-          pointHoverBorderColor: "rgba(159,204,0, 1)"
-        },"rgba(250,109,33,0.5)",'#9a9a9a',"rgb(233,177,69)"
-      ];
-
-
-   
-
+    $scope.months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     
     
 }])
